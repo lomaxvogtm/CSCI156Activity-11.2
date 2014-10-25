37926ed5d81218269d0e60a9a7e407cbe995888b
lomaxvogtm <mcl9@alfred.edu>
2014-10-25 12:27:00 -0400

    Activity 8.1

---
+++ 11.2.py
@@ -0,0 +1,12 @@
+__author__ = 'Madeleine'
+
+Transcript = {'Fall 2013': [['CHEM', '105', '01', 'General Chemistry I'], ['PHIL', '214', '01', 'FYE Ethics']
+             ['MUSI', '215', '01', 'Symphonic Band'], ['FREN', '101', '02', 'French I']],
+              'Spring 2014': [['CHEM', '106', '01', 'General Chemistry I'], ['CHEM', '313', '01', 'Environmental Chemistry']
+             ['MUSI', '217', '01', 'Jazz Band'], ['FREN', '102', '01', 'French II']],
+              'Fall 2014': [['CHEM', '313', '01', 'Organic Chemistry I'], ['CSCI', '156', '01', 'GitHub Appreciation']
+             ['PHYS', '125', '01', 'Physics I'], ['FREN', '201', '02', 'French III']],
+              [['CHEM', '316', '01', 'Organic Chemistry II'], ['PHYS', '126', '01', 'Physics II']
+             ['FREN', '202', '01', 'French IV'], ['BIOL', '345', '01', 'Human Disease']]}
+
+
